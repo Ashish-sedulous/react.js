6abbe6bdc8a170be36c6f62327708214c1e132a5
Ashish <ashishkumar12340sss@gmail.com>
2025-01-21 11:57:00 +0000

    changes

--- 01vitreact/src/main.jsx
+++ 01vitreact/src/main.jsx
@@ -1,9 +1,42 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function Myapp(){
+    return (
+        <div>
+            <h1>Custom app | chai!</h1>
+        </div>
+    )
+}
+
+// const reactElement={
+//     type:'a',
+//     props:{
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google',
+// }
+
+const anotheruser=" chai aur react"
+
+const reactElement=React.createElement(
+    'a',
+    {href:'https://google.com',target: '_blank'},
+    'click me to visit google',
+    anotheruser
+)
+
+const anotherelement=(
+   <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
 createRoot(document.getElementById('root')).render(
   
-    <App />
-  
+    // <Myapp />
+    // anotherelement
+    // reactElement
+    // reactElement
+     <App />
 )
